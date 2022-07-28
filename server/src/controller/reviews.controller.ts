54a53c3d8c7a9dadc9eab9db3b0392cf4c69b8d9
reuven <reubenachladive@gmail.com>
2022-07-28 15:19:55 +0300

    finsh to create contoller for Reviews

--- server/src/controller/reviews.controller.ts
+++ server/src/controller/reviews.controller.ts
@@ -1,1 +1,49 @@
-//
+import { NextFunction, Request, Response } from "express";
+import { IReviews } from "../interfaces/Review.interface";
+import * as ReviewsService from "../services/reviews.service";
+export const getAllReviews = async (
+  req: Request,
+  res: Response<any, any>,
+  next: NextFunction
+): Promise<Response<any, IReviews[]>> => {
+  try {
+    const reviews = await ReviewsService.getAllReviews();
+    return res.send(reviews);
+  } catch (error) {
+    return res.send({ error });
+  }
+};
+export const deleteReviewById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { _id } = req.body;
+    await ReviewsService.deleteOneReview(_id);
+    return res.send({ message: "success" });
+  } catch (error) {
+    return res.status(404).send({ message: "failed" });
+  }
+};
+
+export const createOneReview = async (
+  req: Request<any, IReviews>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { title, description }: IReviews = req.body;
+    if (!title.length || !description.length)
+      return res
+        .status(404)
+        .send({ message: "title or description can't be empty" });
+    const addReview = await ReviewsService.createOneReview({
+      title,
+      description,
+    });
+    res.send(addReview);
+  } catch (error: any) {
+    return res.status(404).send({ message: error.message });
+  }
+};
